77910cbe9b4d99587b03968759892ece25b3b38e
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-07-24 18:37:54 +0200

    test

---
+++ eslint.config.mjs
@@ -0,0 +1,241 @@
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+import eslint from "@eslint/js";
+import defaultStylisticPlugin from "@stylistic/eslint-plugin";
+import javascriptStylisticPlugin from "@stylistic/eslint-plugin-js";
+import typescriptStylisticPlugin from "@stylistic/eslint-plugin-ts";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import pluginImport from "eslint-plugin-import";
+import pluginImportConfig from "eslint-plugin-import/config/recommended.js";
+import prettierPlugin from "eslint-plugin-prettier";
+import globals from "globals";
+
+const projectDirname = dirname(fileURLToPath(import.meta.url));
+
+const env = (() => {
+  if (typeof process.env.NODE_ENV === "undefined") return "default";
+  if (process.env.NODE_ENV === "development") return "development";
+  if (process.env.NODE_ENV === "production") return "production";
+  return "error";
+})();
+
+const allTsExtensionsArray = ["ts", "mts", "cts", "tsx", "mtsx"];
+const allJsExtensionsArray = ["js", "mjs", "cjs", "jsx", "mjsx"];
+const allTsExtensions = allTsExtensionsArray.join(",");
+const allJsExtensions = allJsExtensionsArray.join(",");
+const allExtensions = [...allTsExtensionsArray, ...allJsExtensionsArray].join(
+  ","
+);
+
+const importRules = {
+  "import/no-unresolved": "error",
+  "sort-imports": [
+    "error",
+    {
+      allowSeparatedGroups: true,
+      ignoreCase: true,
+      ignoreDeclarationSort: true,
+      ignoreMemberSort: false,
+      memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+    },
+  ],
+  "import/order": [
+    "error",
+    {
+      groups: [
+        "builtin", // Built-in imports (come from NodeJS native) go first
+        "external", // External imports
+        "internal", // Absolute imports
+        "parent", // Relative imports
+        "sibling", // Relative imports
+        // ['sibling', 'parent'], // Relative imports, the sibling and parent types they can be mingled together
+        "index", // index imports
+        "type", // type imports
+        "unknown", // unknown
+      ],
+      "newlines-between": "always",
+      alphabetize: {
+        order: "asc",
+        caseInsensitive: true, // ignore case
+      },
+    },
+  ],
+};
+
+const baseRules = {
+  "prettier/prettier": "warn",
+  "@stylistic/max-len": [
+    "warn",
+    {
+      code: 120,
+      ignoreComments: true,
+      ignoreTrailingComments: true,
+      ignoreStrings: true,
+      ignoreUrls: true,
+    },
+  ],
+  "@stylistic/indent": ["error", 2, { SwitchCase: 1 }],
+  "@stylistic/semi": ["error", "never"],
+  "@stylistic/quotes": [
+    "warn",
+    "single",
+    { avoidEscape: true, allowTemplateLiterals: false },
+  ],
+  "@stylistic/object-curly-spacing": ["warn", "always"],
+  "@stylistic/array-element-newline": ["error", "consistent"],
+};
+
+const typescriptRules = {
+  ...prettierConfig.rules,
+  ...pluginImportConfig.rules,
+  ...typescriptEslintPlugin.configs.recommended.rules,
+  ...typescriptEslintPlugin.configs["recommended-type-checked"].rules,
+  ...typescriptEslintPlugin.configs.strict.rules,
+  ...typescriptEslintPlugin.configs["strict-type-checked"].rules,
+  ...typescriptEslintPlugin.configs["stylistic-type-checked"].rules,
+  ...typescriptStylisticPlugin.configs["disable-legacy"].rules,
+  ...importRules,
+  ...baseRules,
+};
+
+const javascriptRules = {
+  ...prettierConfig.rules,
+  ...pluginImportConfig.rules,
+  ...typescriptEslintPlugin.configs.recommended.rules,
+  ...typescriptEslintPlugin.configs.strict.rules,
+  ...typescriptEslintPlugin.configs["stylistic"].rules,
+  ...javascriptStylisticPlugin.configs["disable-legacy"].rules,
+  ...importRules,
+  ...baseRules,
+};
+
+const typescriptRulesDev = {
+  "@typescript-eslint/no-explicit-any": ["warn"],
+  "@typescript-eslint/no-unused-vars": ["warn"],
+  "@typescript-eslint/prefer-nullish-coalescing": ["off"],
+  "@typescript-eslint/no-inferrable-types": ["off"],
+  "@typescript-eslint/dot-notation": ["off"],
+};
+
+const javascriptRulesDev = {
+  "@typescript-eslint/no-unused-vars": ["warn"],
+};
+
+const config = [
+  {
+    /* setup parser for all files */
+    files: [`**/*.{${allExtensions}}`],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      parserOptions: {
+        ecmaVersion: "latest", // 2024 sets the ecmaVersion parser option to 15
+        tsconfigRootDir: resolve(projectDirname),
+        project:
+          env === "production" ? "./tsconfig.prod.json" : "./tsconfig.json",
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    /* all typescript files, except config files */
+    files: [`**/*.{${allTsExtensions}}`],
+    ignores: [`**/*.config.{${allTsExtensions}}`],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      "@stylistic": defaultStylisticPlugin,
+      import: pluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptRules,
+      ...typescriptRulesDev,
+    },
+  },
+  {
+    /* all javascript files, except config */
+    files: [`**/*.{${allJsExtensions}}`],
+    ignores: [`**/*.config.{${allJsExtensions}}`],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      "@stylistic": defaultStylisticPlugin,
+      import: pluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...javascriptRules,
+      ...javascriptRulesDev,
+    },
+  },
+  {
+    /* config files: typescript */
+    files: [`**/*.config.{${allTsExtensions}}`],
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      "@stylistic": defaultStylisticPlugin,
+      import: pluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptRules,
+      // '@typescript-eslint/prefer-nullish-coalescing': ['off'],
+    },
+  },
+  {
+    /* config files: javascript */
+    files: [`**/*.config.{${allJsExtensions}}`],
+    settings: {
+      "import/parsers": {
+        espree: [".js", ".mjs", ".cjs", ".jsx", ".mjsx"],
+      },
+      "import/resolver": {
+        typescript: {},
+        // node: {},
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      "@stylistic": defaultStylisticPlugin,
+      import: pluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...javascriptRules,
+      "@typescript-eslint/no-unsafe-member-access": ["off"],
+      "@typescript-eslint/no-unsafe-assignment": ["off"],
+    },
+  },
+
+  {
+    ignores: [
+      "dist",
+      "build",
+      "**/*_lintignore*",
+      "**/*-buildignore*",
+      "**/*_buildignore*",
+    ],
+  },
+];
+
+export default config;
---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+let greetingName = "John";
+
+if (greetingName === "John") {
+  console.log("Hello, John!");
+} else {
+  console.log("Hello, World!");
+}
